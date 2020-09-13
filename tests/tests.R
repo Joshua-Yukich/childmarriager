79b6853c3875b45bd2f9aa5b330cc83dbb65b4ac
Joshua-Yukich <56749858+Joshua-Yukich@users.noreply.github.com>
2020-09-13 00:31:05 -0500

    update tests file to properly reflect new package approach

--- tests/tests.R
+++ tests/tests.R
@@ -1,20 +1,23 @@
 
 library(childmarriager)
-prob_mar_comp <- data(emp_haz_mal)
+data("emp_haz_mal")
+prob_mar_comp <- prob_mar
+pad <- data.frame(child_age_years = seq(0,9, by =1), marrige_prob_perc = rep(0, times = 10))
+prob_mar_comp <- rbind(pad, prob_mar_comp)
+prob_mar_comp[prob_mar_comp$child_age_years >= 18, "marrige_prob_perc"] <- 0
 
-
-cohorts <- cohort_construct(start_year = 2013, end_year = 2014, young_age = 0, old_age = 0)
+cohorts <- cohort_construct(start_year = 2013, end_year = 2019, young_age = 8, old_age = 12)
 
 cohorts[["length_of_sim"]]
+cohorts[["start_ages"]][1]
 
-runs <- params_define(start_year = 2013, end_year = 2014, young_age = 0, old_age = 0,
-               country = "MWI", mar_prob = prob_mar_comp , impact, sex = 0)
+mar_haz_define(start_year = 2013, end_year = 2019, young_age = 8, old_age = 12, mar_prob = prob_mar_comp)
 
-trans <- trans_define(start_year = 2013, end_year = 2014, young_age = 0, old_age = 0,
-              country = "MWI", mar_prob = prob_mar_comp , sex = 0)
 
-strats <- strats_define(start_year = 2013, end_year = 2014, young_age = 0, old_age = 0,
-              country = "MWI", mar_prob = prob_mar_comp , sex = 0)
-
-res <- model_runs(start_year = 2013, end_year = 2033, young_age = 0, old_age = 2,
+res <- model_runs(start_year = 2018, end_year = 2035, young_age = 8, old_age = 18,
             country = "MWI", mar_prob = prob_mar_comp, impact = 1.1, sex = 0)
+
+count_extract(res, strategy = "standard")
+
+
+plot(res[[1]])
