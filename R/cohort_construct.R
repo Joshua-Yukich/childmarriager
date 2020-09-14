eaafc364a1ee05c4d33eb79efa5fa88201201cb9
Joshua-Yukich <56749858+Joshua-Yukich@users.noreply.github.com>
2020-09-13 22:03:32 -0500

    refactor experiment to start cohorts once per year all at age zero

--- R/cohort_construct.R
+++ R/cohort_construct.R
@@ -6,12 +6,13 @@
 #' @param old_age highest starting age in simulation in years
 
 
-cohort_construct <- function(start_year, end_year, young_age, old_age) {
+cohort_construct <- function(start_year, end_year) {
 
-  max_age <- end_year - start_year + old_age
+
   length_of_sim <- end_year - start_year
-  n_cohorts <- old_age - young_age + 1
-  start_ages <- seq(from = young_age, to = old_age, by = 1)
+  n_cohorts <- end_year - start_year - 1
+  start_years <- seq(from = start_year, to = end_year - 1, by = 1)
 
-  list("length_of_sim" = length_of_sim, "n_cohorts" = n_cohorts, "start_ages" = start_ages, "max_age" = max_age)
+  list("length_of_sim" = length_of_sim, "n_cohorts" = n_cohorts, "start_years" = start_years, "end_year" = end_year)
+
 }
