2f6982f82ae63dd74501b3fa517d3631f880cf82
Joshua-Yukich <56749858+Joshua-Yukich@users.noreply.github.com>
2020-09-13 22:04:00 -0500

    adapt to add a marriage hazard profile unique to each cohort

--- R/mar_haz_define.R
+++ R/mar_haz_define.R
@@ -2,19 +2,18 @@
 #'
 #' @param start_year year of simulation start.
 #' @param end_year year of simulation end
-#' @param young_age lowest starting age in simulation in years
-#' @param old_age highest starting age in simulation in years
-#' @param mar_haz data.frame of marriage probaiblities by year (Starting at zero) thru age 24
+#' @param mar_prob list of data.frames of marriage probabilities by year (Starting at zero) thru maximum age
+#' one for each cohort
 
-mar_haz_define <- function(start_year, end_year, young_age, old_age, mar_prob) {
+mar_haz_define <- function(start_year, end_year, mar_prob) {
 
-  cohorts <- cohort_construct(start_year = start_year, end_year = end_year, young_age = young_age, old_age = old_age)
+  cohorts <- cohort_construct(start_year = start_year, end_year = end_year)
 
     mar_haz_list  <- list()
 
     for (i in 1:cohorts[["n_cohorts"]]) {
 
-      mar_haz_list[[i]] <- mar_prob[(cohorts[["start_ages"]][i] + 1):cohorts[["max_age"]],"marrige_prob_perc"]
+      mar_haz_list[[i]] <- mar_prob[[i]][0:(end_year - (cohorts[["start_years"]][i])),"marrige_prob_perc"]
 
     }
 
